fe955cd81ee6e7b3719c07f2dd032743e15088a0
Guang <jouyang19@proton.me>
2024-05-26 18:38:21 -0400

    complete lab

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import NavBar from "../components/NavBar";
+
+export default function About() {
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <h1>This is my about component!</h1>
+      </main>
+    </>
+  );
+}
